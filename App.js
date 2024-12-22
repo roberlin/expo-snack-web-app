72c13fd333f540c78d60d91c25304e0f69902914
Bob <rechnungschmidt@googlemail.com>
2024-12-22 10:47:48 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Willkommen zu meiner ersten Expo-Web-App! 🎉
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f8f8f8",
+  },
+  text: {
+    fontSize: 24,
+    color: "#333",
+    textAlign: "center",
+  },
+});
